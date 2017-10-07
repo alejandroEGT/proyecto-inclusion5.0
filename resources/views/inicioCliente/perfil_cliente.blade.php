7bf778cd61c081a1ba14e1f8d3865924d6d62ab2
davidluna1995 <david.luna.cuevas@gmail.com>
2017-10-07 00:51:42 -0300

    perfil mis datos

--- resources/views/inicioCliente/perfil_cliente.blade.php
+++ resources/views/inicioCliente/perfil_cliente.blade.php
@@ -14,56 +14,148 @@
 			</div>
 		</div>
 
-		<div class="col-md-10">	
-			<div class="tab-content" id="v-pills-tabContent">
+		<div class="col-md-3">
+			<br><div class="card" style="width: 100%">
+			 <img class="card-img-top" src="../foto_productos/1507269297.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h5 class="card-text misDatos">Nombre:</h5>
+			    <p class="card-text">{{Auth::user()->nombres}}</p>
+			    <h5 class="card-text misDatos">Apellidos:</h5>
+			    <p class="card-text">{{Auth::user()->apellidos}}</p>
+			    <h5 class="card-text misDatos">Correo electronico:</h5>
+			    <p class="card-text">{{Auth::user()->email}}</p>
+			  </div>
+			</div>
+		</div>
+
+
+		
+		<div class="col-md-6">	
+
+			<br><div class="tab-content" id="v-pills-tabContent">
 				<!--Mis Datos-->
 			  <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"> 	
 				<div id="accordion" role="tablist">
+
 				  <div class="card">
 				    <div class="card-header" role="tab" id="headingOne">
 				      <h5 class="mb-0">
 				        <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-				          Collapsible Group Item #1
+				          Cambiar correo electronico:
 				        </a>
 				      </h5>
 				    </div>
 
-				    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+				    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
 				      <div class="card-body">
-				        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+				  			<form action="" method="">
+				  				{{csrf_field()}}
+
+							  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Nuevo correo</label>
+							    <div class="col-sm-5">  
+							       <input type="email" class="form-control" id="exampleInputEmail1" name="correo" placeholder="email@ejemplo.cl">
+							    </div>
+							  </div>
+
+							  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Repita correo</label>
+							    <div class="col-sm-5">
+							      <input type="email" class="form-control" id="exampleInputEmail1" name="correo" placeholder="email@ejemplo.cl">
+							    </div>
+							  </div>
+
+						      <div class="boton-sesion row">	
+				  				<button type="submit" class="btn btn-primary btn-outline-success">Guardar</button>
+							  </div>
+						  
+							</form>
 				      </div>
 				    </div>
 				  </div>
+
 				  <div class="card">
 				    <div class="card-header" role="tab" id="headingTwo">
 				      <h5 class="mb-0">
 				        <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-				          Collapsible Group Item #2
+				          Cambiar contraseña:
 				        </a>
 				      </h5>
 				    </div>
+
 				    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
 				      <div class="card-body">
-				        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+				    		<form action="" method="">
+				  				{{csrf_field()}}
+
+				  			  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Contraseña antigua</label>
+							    <div class="col-sm-5">  
+							       <input type="password" class="form-control" id="exampleInputEmail1" name="correo" placeholder="******">
+							    </div>
+							  </div>
+
+							  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Nuevo contraseña</label>
+							    <div class="col-sm-5">  
+							       <input type="password" class="form-control" id="exampleInputEmail1" name="correo" placeholder="******">
+							    </div>
+							  </div>
+
+							  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Repita contraseña</label>
+							    <div class="col-sm-5">
+							      <input type="password" class="form-control" id="exampleInputEmail1" name="correo" placeholder="******">
+							    </div>
+							  </div>
+
+						      <div class="boton-sesion row">	
+				  				<button type="submit" class="btn btn-primary btn-outline-success">Guardar</button>
+							  </div>
+						  
+							</form>
 				      </div>
 				    </div>
 				  </div>
+
 				  <div class="card">
 				    <div class="card-header" role="tab" id="headingThree">
 				      <h5 class="mb-0">
 				        <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-				          Collapsible Group Item #3
+				          Cambiar numero de telefono:
 				        </a>
 				      </h5>
 				    </div>
+
 				    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
 				      <div class="card-body">
-				        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+				       	<form action="" method="">
+				  				{{csrf_field()}}
+
+							  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Nuevo  N° Telefono</label>
+							    <div class="col-sm-5">  
+							       <input type="number" class="form-control" id="exampleInputEmail1" name="correo" placeholder="+56998765432">
+							    </div>
+							  </div>
+
+							  <div class="form-group row">
+							    <label for="staticEmail" class="col-sm-3 col-form-label">Repita N° Telefono</label>
+							    <div class="col-sm-5">
+							      <input type="number" class="form-control" id="exampleInputEmail1" name="correo" placeholder="+56998765432">
+							    </div>
+							  </div>
+
+						      <div class="boton-sesion row">	
+				  				<button type="submit" class="btn btn-primary btn-outline-success">Guardar</button>
+							  </div>
+						  
+						</form>
 				      </div>
 				    </div>
 				  </div>
+
 				</div>  	
-				
 			  </div>
 				<!--Mis Compras-->
 			  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
